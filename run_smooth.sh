c242696094d04eb8e76a946caead386e892e496c
harshit23236 <harshitanand0804@gmail.com>
2026-05-02 13:18:25 +0530

    Cleanup: removed all legacy border smoothing and unused mask files

--- run_smooth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Border smoothing script
-
-python border_smoothing_inference.py \
-  --edited_video /home/harshit23236/LoRAEdit/processed_data/video_1776166954/edited_video.mp4 \
-  --original_video /home/harshit23236/LoRAEdit/processed_data/video_1776166484/source_frames \
-  --mask_video /home/harshit23236/LoRAEdit/processed_data/video_1776166954/inference_mask.mp4 \
-  --output output/smooth.mp4 \
-  --blend_strength 0.6
